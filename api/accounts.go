109701552e065b60cb2930a7ecfc79c7e6830ef0
o-ifeanyi <onuifeanyi95@gmail.com>
2022-10-15 21:41:56 +0100

    Create accounts.go

---
+++ api/accounts.go
@@ -0,0 +1,89 @@
+package api
+
+import (
+	"database/sql"
+	"net/http"
+	db "simplebank/db/sqlc"
+
+	"github.com/gin-gonic/gin"
+)
+
+type createAccountReq struct {
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required,oneof=USD NGN GHS"`
+}
+
+func (server *Server) createAccount(c *gin.Context) {
+	var req createAccountReq
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, server.errorResponse(err))
+		return
+	}
+
+	arg := db.CreateAccountParams{
+		Owner:    req.Owner,
+		Balance:  0,
+		Currency: req.Currency,
+	}
+
+	acc, err := server.store.CreateAccount(c, arg)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, server.errorResponse(err))
+		return
+	}
+
+	c.JSON(http.StatusOK, acc)
+}
+
+type getAccountReq struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getAccount(c *gin.Context) {
+	var req getAccountReq
+
+	if err := c.ShouldBindUri(&req); err != nil {
+		c.JSON(http.StatusBadRequest, server.errorResponse(err))
+		return
+	}
+
+	acc, err := server.store.GetAccount(c, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			c.JSON(http.StatusNotFound, server.errorResponse(err))
+			return
+		}
+		c.JSON(http.StatusInternalServerError, server.errorResponse(err))
+		return
+	}
+
+	c.JSON(http.StatusOK, acc)
+}
+
+type listAccountsReq struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=10,max=20"`
+}
+
+func (server *Server) listAccounts(c *gin.Context) {
+	var req listAccountsReq
+
+	if err := c.ShouldBindQuery(&req); err != nil {
+		c.JSON(http.StatusBadRequest, server.errorResponse(err))
+		return
+	}
+
+	arg := db.ListAccountsParams{
+		Limit:  req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
+
+	accs, err := server.store.ListAccounts(c, arg)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, server.errorResponse(err))
+		return
+	}
+
+	c.JSON(http.StatusOK, accs)
+}
