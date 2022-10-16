c19bb72c4bda6c26167c07c0188fad454647e248
o-ifeanyi <onuifeanyi95@gmail.com>
2022-10-16 23:43:31 +0100

    Update accounts.go

--- api/accounts.go
+++ api/accounts.go
@@ -10,7 +10,7 @@ import (
 
 type createAccountReq struct {
 	Owner    string `json:"owner" binding:"required"`
-	Currency string `json:"currency" binding:"required,oneof=USD NGN GHS"`
+	Currency string `json:"currency" binding:"required,currency"`
 }
 
 func (server *Server) createAccount(c *gin.Context) {
